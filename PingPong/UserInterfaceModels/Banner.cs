1bd60ae91986d9c6aceb264ab6956b29fd70f005
Vera Weikel <vera.weikel@gmail.com>
2023-02-21 08:25:14 -0800

    Commit program structure files.

---
+++ PingPong/UserInterfaceModels/Banner.cs
@@ -0,0 +1,25 @@
+namespace PingPong.UserInterfaceModels
+{
+  class WelcomeBanner
+  {
+    public static string Welcome { get; } = @"
+  #####                                #######                                           
+ #     # #    #   ##   #####  ######      #    #####    ##    ####  #    # ###### #####  
+ #       #    #  #  #  #    # #           #    #    #  #  #  #    # #   #  #      #    # 
+  #####  ###### #    # #    # #####       #    #    # #    # #      ####   #####  #    # 
+       # #    # ###### #####  #           #    #####  ###### #      #  #   #      #####  
+ #     # #    # #    # #      #           #    #   #  #    # #    # #   #  #      #   #  
+  #####  #    # #    # #      ######      #    #    # #    #  ####  #    # ###### #    # 
+                                                                                         ";
+  }
+
+  class GoodbyeBanner
+  {
+    // GoodbyeBanner code here.
+  }
+
+  class SubMenuBanner
+  {
+    // SubMenuBanner code here.
+  }
+}
